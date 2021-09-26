945c4eabb191a838bad49030a88a40c7e6ca27f2
manhhnv <manh117bg@gmail.com>
2021-09-27 01:40:04 +0700

    waitGroup $ json basic

--- concurrency/goroutines/gorountines.go
+++ concurrency/goroutines/gorountines.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func say(message string, n int)  {
+func say(message string, n int, waitGroup *sync.WaitGroup)  {
 	for i := 0; i < n; i++ {
 		time.Sleep(time.Second)
 		fmt.Println(message)
 	}
+	waitGroup.Done()
 }
 
 func main() {
-	go say("Message 1", 10)
-	say("Message 2", 2)
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(2)
+	go say("Message 1", 10, &waitGroup)
+	go say("Message 2", 2, &waitGroup)
+	waitGroup.Wait()
+	fmt.Println("Main func is done")
 }
